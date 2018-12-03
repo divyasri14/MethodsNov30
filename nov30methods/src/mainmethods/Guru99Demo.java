c0bcce1ac952e9e779af4bbb3255d69c43cefdb6
ambikaraj <ambikaraj@LAPTOP-9SCENFI4>
2018-12-03 22:37:50 +0530

    Addcustomer to Guru project

---
+++ nov30methods/src/mainmethods/Guru99Demo.java
@@ -0,0 +1,20 @@
+package mainmethods;
+
+public class Guru99Demo {
+	
+
+public static void main (String[]args) {
+	
+	Guru99Login GL = new Guru99Login ();
+	 GL. invokebrowser ();
+	 GL.getwindowtitle();
+	 GL.Guru99login("mngr166186","tyredet");
+	 GL.newcustomer();
+	 
+	 
+	 
+	 
+	 
+}
+
+}
---
+++ nov30methods/src/mainmethods/Guru99Login.java
@@ -0,0 +1,97 @@
+package mainmethods;
+
+
+	import java.awt.Dimension;
+
+	import org.openqa.selenium.By;
+	import org.openqa.selenium.chrome.ChromeDriver;
+	
+	public class Guru99Login {
+
+		
+		//Shotr cut chrome ctr+space bar and here declaring the varible for chrome driver
+		
+		ChromeDriver driver;
+		
+		//Creating a methos
+		
+		public void invokebrowser(){
+			
+		//	webdriver.chrome.driver this is varibile where it will invoke the path of chrome and this value at system level
+			
+			System.setProperty("webdriver.chrome.driver","C:\\Users\\ambikaraj\\eclipse-workspace\\chromedriver02122018\\chromedriver.exe");
+			
+		//instantiation of object
+			driver = new ChromeDriver();
+			
+		//For responsive testing we have to give dimensions
+//			Dimension dim 	= new  Dimension(400, 526);
+//			
+	////	
+//			driver.manage().window().setSize( dim);
+	//	
+		//To maximize the chrome
+			driver.manage().window().maximize();
+			
+		//To clear the cookies(donot delete the cookies but bypass the cookies
+			
+			driver.manage().deleteAllCookies();
+		//To call the web page we should give protolcals  like  http,http
+			
+			driver.get("http://demo.guru99.com/v4/");
+			
+		}
+		
+		
+		public void getwindowtitle() {
+			
+		
+	System.out.println(driver.getTitle());
+			
+			
+		}
+		
+		
+		public void Guru99login(String userid ,String password) {
+			
+			driver.findElement(By.name("uid")).sendKeys(userid);
+			driver.findElement(By.name("password")).sendKeys(password);
+			driver.findElement(By.name("btnLogin")).click();
+			
+					}
+		public void newcustomer () {
+			
+			driver.findElement(By.linkText("New Customer")).click();
+			
+			
+			
+			driver.findElement(By.name("name")).sendKeys("divya");
+			driver.findElement(By.xpath("//input[@value = 'f']")).click();
+			driver.findElement(By.id("dob")).sendKeys("10-11-1985");
+			driver.findElement(By.name("addr")).sendKeys("divya");
+			driver.findElement(By.name("city")).sendKeys("Banglore");
+			driver.findElement(By.name("state")).sendKeys("Karnataka");
+			driver.findElement(By.name("pinno")).sendKeys("123456");
+			driver.findElement(By.name("telephoneno")).sendKeys("231456");
+			driver.findElement(By.name("emailid")).sendKeys("qq124@gmail.com");
+			driver.findElement(By.name("password")).sendKeys("1234");
+			driver.findElement(By.name("sub")).click();
+		
+		}
+		
+		
+		
+		public void close() {			
+		//Close the current active page	
+			driver.close();
+			
+			//Close the all the windows whih are opend by selenium
+//			driver.quit();
+			
+			
+		}
+		
+		
+	}
+
+	
